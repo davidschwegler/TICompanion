071198e0d333e0303a01613d7b07be32b5784b89
David Schwegler <david.schwegler@logos.com>
2014-05-25 17:51:53 -0700

    Fixing debug date

--- src/src/com/appenjoyment/ticompanion/TIInfo.java
+++ src/src/com/appenjoyment/ticompanion/TIInfo.java
@@ -66,7 +66,7 @@ public final class TIInfo
 	{
 		// Calendar calendar = GregorianCalendar.getInstance(); // TimeZone.getTimeZone("UTC"));
 		// calendar.set(2014 - 1990, GregorianCalendar.JULY, 18, 19, 0);
-		DateTime dateTime = new DateTime(2013, 7, 18, 19, 0, DateTimeZone.UTC);
+		DateTime dateTime = new DateTime(2014, 7, 18, 19, 0, DateTimeZone.UTC);
 		Date2014LocalTime = dateTime.withZone(DateTimeZone.getDefault());
 	}
 }
