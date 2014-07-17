cf249713bdcdf7be4e060186e75c470576822d94
David Schwegler <dschwegler777@gmail.com>
2014-07-17 00:07:32 -0700

    9AM PDT (GMT-7) is 4pm GMT

--- src/src/com/appenjoyment/ticompanion/TIInfo.java
+++ src/src/com/appenjoyment/ticompanion/TIInfo.java
@@ -59,7 +59,7 @@ public final class TIInfo
 	{
 		// Calendar calendar = GregorianCalendar.getInstance(); // TimeZone.getTimeZone("UTC"));
 		// calendar.set(2014 - 1990, GregorianCalendar.JULY, 18, 19, 0);
-		DateTime dateTime = new DateTime(2014, 7, 18, 15, 0, DateTimeZone.UTC);
+		DateTime dateTime = new DateTime(2014, 7, 18, 16, 0, DateTimeZone.UTC);
 		Date2014LocalTime = dateTime.withZone(DateTimeZone.getDefault());
 	}
 }
