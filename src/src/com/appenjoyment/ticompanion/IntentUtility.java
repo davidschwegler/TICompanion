59f347dd4c2d67a416542a016b0385869a01b9e4
David Schwegler <dschwegler777@gmail.com>
2014-07-17 00:07:06 -0700

    Fix multiple instances of the main activity being launched.

--- src/src/com/appenjoyment/ticompanion/IntentUtility.java
+++ src/src/com/appenjoyment/ticompanion/IntentUtility.java
@@ -8,8 +8,7 @@ public final class IntentUtility
 	public static Intent getBringToFrontIntent(Context applicationContext, Class<?> mainLauncherActivity)
 	{
 		return new Intent(applicationContext, mainLauncherActivity)
-				.addCategory(Intent.CATEGORY_LAUNCHER)
-				.setAction(Intent.ACTION_MAIN);
+				.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
 	}
 
 	private IntentUtility()
